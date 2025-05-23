1eb07508d8cb56024d2b3cdc5d142fe965361ea4
Tristan <11915275+ta-vroom@users.noreply.github.com>
2025-05-23 19:26:36 -0400

    add: missing rpmfusion mirrors

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,20 +4,26 @@ set -ouex pipefail
 
 ### Install packages
 
+pkgs=(
+    intel-media-driver
+#   fastfetch firewalld fwupd-efi intel-compute-runtime
+#   intel-media-driver neovim podman-compose rclone starship zsh
+#   cockpit cockpit-networkmanager cockpit-podman cockpit-selinux
+#   cockpit-storaged cockpit-system duperemove samba samba-usershares
+)
+
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+
+dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
+#dnf5 install -y "${pkgs[@]}"
+
+dnf5 install -y intel-media-driver
 
 #### Example for enabling a System Unit File
 
