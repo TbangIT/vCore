f0954657be9f51b2b3e71abc4cde172f1e04c563
Tristan <11915275+ta-vroom@users.noreply.github.com>
2025-05-26 13:39:59 -0400

    add: server packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,27 +4,5 @@ set -ouex pipefail
 
 ### Install packages
 
-pkgs=(
-    intel-media-driver
-#   fastfetch firewalld fwupd-efi intel-compute-runtime
-#   intel-media-driver neovim podman-compose rclone starship zsh
-#   cockpit cockpit-networkmanager cockpit-podman cockpit-selinux
-#   cockpit-storaged cockpit-system duperemove samba samba-usershares
-)
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-
-
-dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf config-manager setopt fedora-cisco-openh264.enabled=1
-#dnf5 install -y "${pkgs[@]}"
-
-dnf5 install -y intel-media-driver
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+/ctx/server-packages.sh
---
+++ build_files/server-packages.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set ${SET_X:+-x} -eou pipefail
+
+
+PKGS=(
+   fastfetch firewalld fwupd-efi intel-compute-runtime
+   intel-media-driver neovim podman-compose rclone starship zsh
+   cockpit cockpit-networkmanager cockpit-podman cockpit-selinux
+   cockpit-storaged cockpit-system duperemove samba samba-usershares
+)
+
+dnf5 -y -q copr enable atim/starship
+dnf5 install -y -q https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm > /dev/null
+
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
+
+/ctx/cockpit_45drives.sh
+
+dnf5 install -y -q "${PKGS[@]}"
+
+# Enable Podman and Cockpit
+systemctl enable podman.socket
+systemctl enable cockpit.socket
