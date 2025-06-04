73b670450559e4d58ead372e888f0b71510b876e
Tristan <11915275+ta-vroom@users.noreply.github.com>
2025-06-03 22:53:18 -0400

    refactor:  sorted tasks

--- build_files/server-packages.sh
+++ build_files/server-packages.sh
@@ -1,24 +1,75 @@
 #!/usr/bin/env bash
 
+RPMFUSION_FREE=https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+RPMFUSION_NONFREE=https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
 set ${SET_X:+-x} -eou pipefail
 
-
-PKGS=(
-   fastfetch firewalld fwupd-efi intel-compute-runtime
-   intel-media-driver neovim podman-compose rclone starship zsh
-   cockpit cockpit-networkmanager cockpit-podman cockpit-selinux
-   cockpit-storaged cockpit-system duperemove samba samba-usershares
+COCKPIT=(
+   cockpit
+   cockpit-networkmanager
+   cockpit-podman
+   cockpit-selinux
+   cockpit-storaged
+   cockpit-system
+   cockpit-ostree
+   # Fonts for Cockpit
+   adwaita-mono-fonts       
+   adwaita-sans-fonts
+   # Aurora Logo for branding
+   aurora-logos
 )
 
+SAMBA=(
+   samba
+   samba-usershares
+)
+
+TERMINAL=(
+   zsh
+   fastfetch
+   starship
+   neovim
+   inotify-tools
+   tree-sitter-cli
+)
+
+PKGS=(
+   libatomic
+   firewalld
+   ipset
+   intel-compute-runtime
+   intel-media-driver
+   podman-compose
+   rclone
+   ripgrep
+   duperemove
+   $COCKPIT
+   $SAMBA
+   $TERMINAL
+)
+
+# Enable Repos
 dnf5 -y -q copr enable atim/starship
-dnf5 install -y -q https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm > /dev/null
+dnf5 install -y -q $RPMFUSION_NONFREE # Needed for intel-media-driver
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=1 # Needed for rpmfusion_*
 
-dnf5 config-manager setopt fedora-cisco-openh264.enabled=1
+# Install 45Drive Cockpit Packages 
+bash /ctx/cockpit_45drives.sh
 
-#/ctx/cockpit_45drives.sh
-
-dnf5 install -y -q "${PKGS[@]}"
+# Install Packages
+dnf5  --refresh \
+      --setopt=install_weak_deps=False \
+      install -y -q "${PKGS[@]}" \
+      --exclude=nodejs-docs,nodejs-full-i18n
 
 # Enable Podman and Cockpit
 systemctl enable podman.socket
 systemctl enable cockpit.socket
+
+#Cleanup
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=0
+dnf5 copr disable atim/starship
+dnf5 copr disable ublue-os/packages
+dnf5 config-manager setopt rpmfusion-nonfree.enabled=0
+dnf5 config-manager setopt rpmfusion-nonfree-updates.enabled=0
