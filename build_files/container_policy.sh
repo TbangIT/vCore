368434710f4db64c2c188638bb1b22e0d7423162
Tristan <11915275+ta-vroom@users.noreply.github.com>
2025-06-04 23:14:30 -0400

    add: container policy for repo-owner

---
+++ build_files/container_policy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Check if the key exists using jq safely (avoid command substitution inside if)
+if jq -e '.transports.docker["ghcr.io/ta-vroom/vos"]' /etc/containers/policy.json > /dev/null; then
+  echo "Policy for ghcr.io/ta-vroom/vos already exists"
+  exit 0
+else 
+  echo "Adding policy for ghcr.io/ta-vroom"
+
+  jq '.transports.docker["ghcr.io/ta-vroom"] = [
+    {
+        "type": "sigstoreRepository",
+        "signedIdentity": {
+            "type": "exactMatch",
+            "identity": "https://github.com/ta-vroom/vos/.github/workflows/build.yml@refs/heads/main"
+        }
+    }
+  ]' /etc/containers/policy.json > /tmp/policy.json
+
+mv /tmp/policy.json /etc/containers/policy.json
+
+  # Validate JSON
+  jq empty /etc/containers/policy.json
+fi
