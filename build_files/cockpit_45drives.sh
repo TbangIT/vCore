71aa0ad28176ffa9b925c2d2fc58398a988fe325
Tristan <11915275+ta-vroom@users.noreply.github.com>
2025-06-04 23:13:47 -0400

    add: 45drives cockpit modules (for testing)

--- build_files/cockpit_45drives.sh
+++ build_files/cockpit_45drives.sh
@@ -1,3 +1,35 @@
-git clone https://github.com/45Drives/cockpit-zfs-manager.git /tmp/cockpit-zfs
-mkdir -p /usr/share/cockpit/zfs
-cp -r /tmp/cockpit-zfs/zfs /usr/share/cockpit
+#!/usr/bin/env bash
+
+# cockpit-zfs-manager is already installed in /usr/share/cockpit, but not as a .rpm
+# not sure if there is any benefit to installing it this way, but here it is
+if false; then
+  dnf5 -y -q copr enable spike/znapzend >/dev/null 2>&1
+  dnf5 -y -q --setopt=install_weak_deps=False install znapzend
+
+  dnf5 -y -q install $(
+    curl -s https://api.github.com/repos/45Drives/cockpit-zfs-manager/releases/latest |
+    jq -r '.assets[] | select(.name | endswith(".rpm")) | .browser_download_url'
+  )
+fi
+
+# This is a RHEL variable, not present in Fedora
+# TODO::Look into automating this 
+RELEASEVER=9
+
+# Install the 45drives repo
+dnf5 config-manager addrepo \
+    --set=baseurl=https://repo.45drives.com/enterprise/rocky/el$RELEAEVER/stable/x86_64 \
+    --id=45drives_enterprise \
+    --set=name="45Drives Enterprise $RELEASEVER Repo" \
+    --set=enabled=1 \
+    --set=gpgcheck=1 \
+    --set=gpgkey=https://repo.45drives.com/key/gpg.asc
+
+# Install other 45drive cockpit modules
+dnf5 install -y -q --setopt=install_weak_deps=False cockpit-45drives-hardware cockpit-navigator cockpit-file-sharing
+
+# Disable znapzend repo
+dnf5 -y copr disable spike/znapzend
+
+# Disable 45drives repo
+dnf5 config-manager setopt 45drives_enterprise.enabled=0 
