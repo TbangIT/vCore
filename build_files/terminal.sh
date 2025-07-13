f3ef9aa190d89b0cf8e9e6e4cbda93d5251905ad
Tristan <11915275+ta-vroom@users.noreply.github.com>
2025-07-13 09:57:23 -0400

    separated rust packages from terminal

--- build_files/terminal.sh
+++ build_files/terminal.sh
@@ -5,11 +5,24 @@ source /ctx/common
 dnf5 -y -q copr enable atim/starship >/dev/null 2>&1 
 dnf5 -y -q copr enable varlad/zellij >/dev/null 2>&1
 
+RUST=(
+    ripgrep # grep
+    fd-find # find
+    bat # cat
+    zoxide # cd
+    dust # du search what takes space
+    duf # df see storage available
+    zellij # tmux
+)
+
+
+PKGS+=( "${RUST[@]}" )
+
 PKGS=(
-   fish
-   fastfetch
-   starship
-   zellij
+   fish # Shell
+   fastfetch # Neofetch alt.
+   rclone 
+   starship 
    neovim
    inotify-tools
    tree-sitter-cli
