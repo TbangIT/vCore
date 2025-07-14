1b0a4c5c82ad3acd8ea23b425501d9786f2c02d9
Tristan <11915275+ta-vroom@users.noreply.github.com>
2025-07-14 19:32:28 -0400

    added bin dir

--- build_files/github.sh
+++ build_files/github.sh
@@ -1,10 +1,9 @@
 #!/bin/env bash
-
-BIN_DIR=/usr/bin
+source /ctx/common
 
 dnf5 install -y -q wget
 
 wget -c https://github.com/eza-community/eza/releases/latest/download/eza_x86_64-unknown-linux-gnu.tar.gz -O - | tar xz
 sudo chmod +x eza
 sudo chown root:root eza
-sudo mv eza /usr/bin/eza
+sudo mv eza "$BIN_DIR/eza"
