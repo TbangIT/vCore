8d1b6367e524d7d4eb5ad7f3027030d7e48ed06e
Tristan <11915275+ta-vroom@users.noreply.github.com>
2025-07-14 09:55:34 -0400

    moved openh264 line

--- build_files/fusion-packages.sh
+++ build_files/fusion-packages.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 source /ctx/common
 
+dnf5 config-manager setopt fedora-cisco-openh264.enabled=1 # Needed for rpmfusion_*
+
 RPMFUSION_FREE=https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 RPMFUSION_NONFREE=https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
@@ -14,7 +16,6 @@ PKGS=()
 PKGS+=( "${INTEL_DRIVERS[@]}" )
 
 # Enable Repos
-dnf5 config-manager setopt fedora-cisco-openh264.enabled=1 # Needed for rpmfusion_*
 #dnf5 install -y -q $RPMFUSION_FREE 
 dnf5 install -y -q $RPMFUSION_NONFREE # Needed for intel-media-driver
 
