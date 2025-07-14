dc1adff4f43d85537300ce9b546ec46189415ff0
Tristan <11915275+ta-vroom@users.noreply.github.com>
2025-07-14 10:49:15 -0400

    fixed nix install command

--- build_files/nix.sh
+++ build_files/nix.sh
@@ -1,41 +1,28 @@
 #!/usr/bin/env bash
 source /ctx/common
 
-cat <<EOF > /etc/tmpfiles.d/prepare-nix-mount.service
-[Unit]
-Description=Create mount point for Nix Store
-Before=nix.mount
+# Create the /nix symlink on the base image
+ln -s /var/nix /nix
 
-[Service]
-Type=oneshot
-ExecStart=/usr/bin/mkdir -p /nix /var/nix
-RemainAfterExit=true
-
-[Install]
-WantedBy=multi-user.target
+# Create a tmpfile config to create the /var/nix directory on boot
+cat <<EOF > /usr/lib/tmpfiles.d/nix.conf
+# Create /var/nix directory
+d /var/nix 0755 root root -
 EOF
 
+# Enable the Nix Repo
+dnf5 -y -q copr enable petersen/nix >/dev/null 2>&1 
 
-cat <<EOF > /usr/lib/systemd/nix.mount
-[Unit]
-Description=Bind mount /var/nix to /nix
-Before=local-fs.target
-
-[Mount]
-What=/var/nix
-Where=/nix
-Type=none
-Options=bind
-
-[Install]
-WantedBy=local-fs.target
-EOF
-
-systemctl enable prepare-nix-mount.service
-systemctl enable nix.mount
-
-dnf5 copr enable petersen/nix
-
+# Install Nix
 dnf5 install -y -q nix
 
+# Enable the Nix Daemon (to enable Nix for all users)
 systemctl enable nix-daemon.service
+
+# Enable Nix MOTD
+cat <<EOF > /etc/motd.d/nix
+Nix has been installed. To use, add current user to nixusers group
+EOF
+
+# Disable the Nix Repo
+dnf5 -y -q copr disable petersen/nix >/dev/null 2>&1 
