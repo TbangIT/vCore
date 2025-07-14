954eb8238ae77048466410302de078f260479ac8
Tristan <11915275+ta-vroom@users.noreply.github.com>
2025-07-14 17:00:08 -0400

    removed the motd and moved symlink to later
    apparently the package creates the /nix folder automatically

--- build_files/nix.sh
+++ build_files/nix.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 source /ctx/common
 
-# Create the /nix symlink on the base image
-ln -s /var/nix /nix
 
 # Create a tmpfile config to create the /var/nix directory on boot
 cat <<EOF > /usr/lib/tmpfiles.d/nix.conf
 # Create /var/nix directory
-d /var/nix 0755 root root -
+d /var/nix 0775 root nix -
 EOF
 
 # Enable the Nix Repo
@@ -20,9 +18,15 @@ dnf5 install -y -q nix
 systemctl enable nix-daemon.service
 
 # Enable Nix MOTD
-cat <<EOF > /etc/motd.d/nix
-Nix has been installed. To use, add current user to nixusers group
-EOF
+# cat <<EOF > /etc/motd.d/nix
+# Nix has been installed. To use, add current user to nix-users group
+# EOF
 
 # Disable the Nix Repo
 dnf5 -y -q copr disable petersen/nix >/dev/null 2>&1 
+
+# Remove the /root nix directory
+rm -rf /nix
+
+# Create the /nix symlink on the base image
+ln -s /var/nix /nix
