4489996ea3f01401840c0b71a5460637af23244c
Tristan <11915275+ta-vroom@users.noreply.github.com>
2025-07-20 20:35:22 -0400

    fixed some booster build errors

--- build_files/booster.sh
+++ build_files/booster.sh
@@ -10,13 +10,16 @@ mkdir -p /tmp/booster
 tar --zstd -xvf /tmp/booster-0.12-1-x86_64.pkg.tar.zst -C /tmp/booster 
 rsync -a /tmp/booster/usr/ /usr/
 
+DNF fido2-tools
+
 cat <<EOF > /etc/booster.yaml
-# https://wiki.archlinux.org/title/Booster
-extra_files: fido2-assert
+# https://man.archlinux.org/man/booster.1
+network:
+  dhcp: on
+extra_files: fido2-assert,/usr/lib/ostree
 EOF
 
 KERNEL=$(ls /usr/lib/modules)
-
-booster build --force "/boot/initramfs-$KERNEL.img" \
+/usr/bin/booster build --force "/boot/initramfs.img" \
   --kernel-version="$KERNEL" \
   --modules-dir="/usr/lib/modules/$KERNEL"
